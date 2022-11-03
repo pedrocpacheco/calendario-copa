25ef3919058af141202306483ccb0bed5c649bc1
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2022-11-03 09:42:39 -0300

    cores e estilo css dinamico

--- main.js
+++ main.js
@@ -8,37 +8,25 @@ function creategame(player1, hour, player2) {
 `;
 }
 
+let delay = -0.3;
 function createCard(date, day, games) {
+  delay = delay + 0.3;
   return `
-    <div class="card">
-                <h2>${date}<span>${day}</span></h2>
-                <ul>
-                    ${games}
-                </ul>
-            </div>
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date}<span>${day}</span></h2>
+        <ul>
+          ${games}
+        </ul>
+    </div>
 `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-            <img src="./assets/logo.svg" alt="logo">
-</header>
-    <main id="cards">
-           ${createCard(
-             "24/11",
-             "quinta",
-             creategame("brazil", "16:00", "serbia")
-           )}
-           ${createCard(
-             "28/11",
-             "segunda",
-             creategame("switzerland", "13:00", "brazil") +
-               creategame("portugal", "16:00", "uruguay")
-           )}
-           ${createCard(
-             "02/12",
-             "sexta",
-             creategame("brazil", "16:00", "cameroon")
-           )}
-        </main>
-`;
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", creategame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    creategame("switzerland", "13:00", "brazil") +
+      creategame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", creategame("brazil", "16:00", "cameroon"));
